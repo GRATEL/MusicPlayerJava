ccc4f6f33095cafa750d921ebaaa8eff71e60439
Gratel <folajide772@gmail.com>
2023-03-06 19:51:21 -0500

    little changggges

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Album album = new Album("Album");
+        Album album = new Album("Album1",artist: "AC/DC" );
+
+
+        album.addsong(title"TNT, duration: 4.5);
+        
     }
 }
