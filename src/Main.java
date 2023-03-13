d92b35b2a3a78cd2bd55d2c388410d142c727663
Gratel <folajide772@gmail.com>
2023-03-13 18:41:59 -0400

    finally

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
         Album album = new Album("Album1",artist: "AC/DC" );
 
 
-        album.addsong(title"TNT, duration: 4.5);
+        album.addsong(title: "TNT, duration: 4.5);
+        album.addsong(title:"Highway to hell", duration: 4.5);
+        album.addsong(title: "Highlife", duration: 4.5);
+        album.addsong(title: "Mario", duration: 4.5);
         
     }
+
 }
